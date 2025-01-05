905c92d7bd53f10c3b2d31693c4038b7f5f42d75
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2025-01-05 15:59:47 +0400

    courser add

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -7,6 +7,15 @@ const Classes = () => {
         paddingLeft:"400px", paddingTop:"50px"
      }}>
         <h1 className='font-bold' style={{paddingLeft:"350px", fontSize:"30px", paddingTop:"30px"}}>Create Course!</h1>
+        <input className='inp' type="text" id="website" name="website" placeholder="Course Name" required />
+        <input className='inp' type="text" id="website" name="website" placeholder="Topics" required />
+
+        <input className='inp' type="text" id="website" name="website" placeholder="Major" required />
+
+        <button  className='btn-creat' style={{ marginLeft: "420px" }}>Create Course</button>
+
+
+
      </div>
     </div>
   )
